512e3f02dc6075e11bd57e0284ad162cda50957a
Osman Abdullayev <os.abdullayev@gmail.com>
2022-10-09 21:07:47 +0400

    after finishing the homework

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import './sass/style.scss';
+import img1 from './img/img1.jpg'
+import img2 from './img/img2.jpg'
+import img3 from './img/img3.jpg'
+import img4 from './img/img4.jpg'
+
+
+class Card extends React.Component {
+	render() {
+		return (
+			<div className = "card">
+				<h1>{this.props.title}</h1>
+				<img src={img1} alt="" />
+				<p>{this.props.content}</p>
+			</div>
+		)
+	}
+}
 
 class App extends React.Component {
 	render() {
 		return (
-			<>
-				<h1>Hellddo</h1>
-			</>
+			<div className="row">
+				<Card title="Hello1" content="This is some text inside of the card."/>
+				<Card title="Hello2" content="This is some info inside of the card."/>
+				<Card title="Hello3" content="This is some topc inside of the card."/>
+				<Card title="Hello3" content="This is some topc inside of the card."/>
+			</div>
 		);
 	}
 }
