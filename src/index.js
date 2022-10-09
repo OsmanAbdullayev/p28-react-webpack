ef5ea6a164ca8594b58441ff502a3751f9aa8b93
Osman Abdullayev <os.abdullayev@gmail.com>
2022-10-09 19:59:22 +0400

    after fixing the bundle issue

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 class App extends React.Component {
 	render() {
 		return (
 			<>
-				<h1>Hello</h1>
+				<h1>Hellddo</h1>
 			</>
 		);
 	}
 }
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
